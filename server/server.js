43fe801052c9f5118337cd942c00f10934395236
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-10 13:21:49 -0600

    Add routes and body parser to server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 7329;
+const koalasRouter = require('./routes/koalas.router');
+
+// body parser
+app.use(bodyParser.urlencoded({extended: true}));
 
 //serve static files
 app.use(express.static('server/public'));
+
+// routes
+app.use('/koalas', koalasRouter);
 
 app.listen(PORT, function(){
     console.log('server running on port:', PORT);
