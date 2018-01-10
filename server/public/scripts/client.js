28ff5749482ce92bbcb4f77fc3c202212458f88d
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-10 14:02:52 -0600

    Display koalas on DOM. Base Mode Complete Yo!

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,7 +15,21 @@ function getKoalas() {
         url: '/koalas',
         success: function(response) {
             console.log('response from GET', response);
-            
+            for (let i = 0; i < response.length; i++) {
+                const koala = response[i];
+                if (koala.ready_to_transfer === true) {
+                    koala.ready_to_transfer = "Yes";
+                } else {
+                    koala.ready_to_transfer = "No";
+                }
+                let $listItem = $('<li>');
+                $listItem.append('<h2>' + koala.name + '</h2>');
+                $listItem.append('<p class="age">Age: ' + koala.age + '</p>');
+                $listItem.append('<p class="gender">Gender: ' + koala.gender + '</p>');
+                $listItem.append('<p class="transferrable">Ready to Transfer? ' + koala.ready_to_transfer + '</p>');
+                $listItem.append('<p class="notes">Notes: ' + koala.notes + '</p>');
+                $('#koalaList').prepend($listItem);
+            }
         }
     });
 }
