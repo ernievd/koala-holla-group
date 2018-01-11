19d02baf7c6b7deb945d718fc74a48d36c944422
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-01-11 08:31:12 -0600

    Planning on edit portion

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -35,7 +35,7 @@ function getKoalas() {
                 $listItem.append(`<p class="age" data-age="${koala.age}">Age: ${koala.age}</p>`);
                 $listItem.append(`<p class="gender" data-gender="${koala.gender}">Gender: ${koala.gender}</p>`);
                 $listItem.append(`<p class="transferrable" data-transfer="${koala.ready_to_transfer}">Ready to Transfer? ${koala.ready_to_transfer}</p>`);
-                $listItem.append(`<p class="notes" data-notes="${koala.notes}">Notes: ${koala.notes} </p>`);
+                $listItem.append(`<p class="notes">${koala.notes}</p>`);
                 $listItem.append(transferButton);
                 $listItem.append('<button class="deleteButton btn btn-info">Delete</button>');
                 $listItem.append('<button class="editButton btn btn-info">Edit</button>');
@@ -96,27 +96,48 @@ function deleteKoala() {
     });
 }
 
+let editedKoala = {};
+
 function editKoala() {
-    let koalaName = $(this).parents('.koalaItem').find('.name').text();
-    $(this).parents('.koalaItem').find('.name').replaceWith(`<input value=${koalaName} />`);
+    // Get the existing data and update the fields to inputs
+    let koalaName = $(this).parents('.koalaItem').find('.name').data('name');
+    $(this).parents('.koalaItem').find('.name').replaceWith(`<p>Name:</p><input class="new-name" value="${koalaName}" />`);
 
     let koalaAge = $(this).parents('.koalaItem').find('.age').data('age');
-    $(this).parents('.koalaItem').find('.age').replaceWith(`<input value="${koalaAge}" />`);
+    $(this).parents('.koalaItem').find('.age').replaceWith(`<p>Age:</p><input class="new-age" value="${koalaAge}" />`);
 
-    let koalaGender = $(this).parents('.koalaItem').find('.gender').text();
-    $(this).parents('.koalaItem').find('.gender').replaceWith(`<input value=${koalaGender} />`);
+    let koalaGender = $(this).parents('.koalaItem').find('.gender').data('gender');
+    $(this).parents('.koalaItem').find('.gender').replaceWith(`<p>Gender:</p><input class="new-gender" value="${koalaGender}" />`);
 
-    let koalaTransfer = $(this).parents('.koalaItem').find('.transferrable').text();
-    $(this).parents('.koalaItem').find('.transferrable').replaceWith(`<input value=${koalaTransfer} />`);
+    let koalaTransfer = $(this).parents('.koalaItem').find('.transferrable').data('transfer');
+    $(this).parents('.koalaItem').find('.transferrable').replaceWith(`<p>Ready To Transfer:</p><input class="new-transferrable" value="${koalaTransfer}" />`);
     
     let koalaNotes = $(this).parents('.koalaItem').find('.notes').text();
-    $(this).parents('.koalaItem').find('.notes').replaceWith(`<input value=${koalaNotes} />`);
+    $(this).parents('.koalaItem').find('.notes').replaceWith(`<p>Notes:</p><input class="new-notes" value="${koalaNotes}" />`);
 
-    // let editedKoala = {
-    //     name: $('#koalaName').val(),
-    //     gender: $('#koalaGender').val(),
-    //     age: $('#koalaAge').val(),
-    //     transferrable: $('#readyToTransfer').val(),
-    //     notes: $('#koalaNotes').val() 
-    // }
+    // update the editedKoala object to get ready to send the updated info
+    editedKoala = {
+        name: $(this).parents('.koalaItem').find('.new-name').val(),
+        gender: $(this).parents('.koalaItem').find('.new-age').val(),
+        age: $(this).parents('.koalaItem').find('.new-gender').val(),
+        transferrable: $(this).parents('.koalaItem').find('.new-transferrable').val(),
+        notes: $(this).parents('.koalaItem').find('.new-notes').val() 
+    }
+    
+    console.log(editedKoala);
+    
+
+    // hide the Ready for Transfer and Delete Buttons
+
+    // Decide on behavior for multiple edit buttons pressed
+    
+    // add a Submit button
+}
+
+function submitEditedKoala() {
+    // hide edit button
+
+    // show Ready for Transfer and Delete buttons
+
+    // PUT request to update the koala
 }
